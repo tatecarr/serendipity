fce1a0c82b9360dcf8208549d7295f1fdc3c92bf
Taylor Carr <tate.carr@gmail.com>
2013-03-27 16:45:45 -0400

    generalized loaded user to id of logged in user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,43 @@ class HomeController < ApplicationController
 	def index
 
 
+		the_id = params['uid'] || current_user.uid
+
+		# user = FbGraph::User.me(current_user.access_token)
+
+		user = FbGraph::User.fetch(the_id)
+
+		puts 'User-----', user.inspect, '-----User'
+
+		@picture_url = user.picture
+
+puts user.inspect
+
+
+
+		user = FbGraph::User.fetch(the_id, :access_token => current_user.access_token)
+
+		@friends = user.friends
+		puts 'Friend-----', @friends.length, '-----Friend'
+
+		# ff_id = @friends.first.raw_attributes['id']
+		# puts 'ff_id',ff_id,''
+		# ff = FbGraph::User.fetch(ff_id, :access_token => current_user.access_token)
+
+		# fffriends = ff.friends
+
+		# puts 'First friend-----', ff.inspect, '-----First friend' #, fffriends.length, '-----ff length'
+
+
+		respond_to do |format|
+			format.html # index.html.erb
+		end
+	end
+
+
+
+	def user_mgmt
+
 		user = FbGraph::User.me(current_user.access_token)
 
 		user = FbGraph::User.fetch('tate.carr')
@@ -12,20 +49,11 @@ class HomeController < ApplicationController
 
 		@picture_url = user.picture
 
-puts user.inspect
 
+		@users = User.all
 
-
-		user = FbGraph::User.fetch('tate.carr', :access_token => current_user.access_token)
-
-		@friends = user.friends
-		puts 'Friend-----', @friends.length, '-----Friend'
-
-
-		respond_to do |format|
-      		format.html # index.html.erb
-      	end
 	end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ Serendipity::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match 'home/user_mgmt' => 'home#user_mgmt'
 
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
