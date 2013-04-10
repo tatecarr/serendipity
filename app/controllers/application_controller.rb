8165c50948b46416937e2b543fb59b2b422a9f4a
Taylor Carr <tate.carr@gmail.com>
2013-04-10 11:12:10 -0400

    checking if current_user exists in the before_filter function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < ActionController::Base
 
   
   def set_user_picture
-  	@picture_url = 'https://graph.facebook.com/'+current_user.uid+'/picture'
+  	if current_user && current_user.uid
+  	 	@picture_url = 'https://graph.facebook.com/'+current_user.uid+'/picture'
+  	 else
+  	 	@picture_url = 'http://www.clker.com/cliparts/5/9/4/c/12198090531909861341man%20silhouette.svg.med.png'
+  	 end
   end
   
 end
